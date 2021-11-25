97c5bf6e77b78054b63aa7808c536870416b42e7
blossom4 <cho8213456@gmail.com>
2021-11-25 15:02:06 +0900

    feat(category): connect product data api sorted by categories

---
+++ ite5-fe-mobile/src/components/index/indexCategoryLSDepth2.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h3 @click="showCategoryLSDepth3">• {{ lskey }}</h3>
+    <div :id="'categoryLSDepth3_' + idx" style="display: none;">
+      <indexCategoryLSDepth3 v-for="(depth3, idx) in categoryLS[lskey]" :key="idx" :depth3="depth3" :lskey="lskey"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import indexCategoryLSDepth3 from '@/components/index/indexCategoryLSDepth3'
+
+export default {
+  name: 'indexCategoryLSDepth2',
+  components: {
+    indexCategoryLSDepth3,
+  },
+  props: {
+    lskey: {
+      type: String
+    },
+    idx: {
+      type: Number
+    },
+  },
+  methods: {
+    showCategoryLSDepth3() {
+      const categoryLSDepth3Id = document.getElementById('categoryLSDepth3_' + this.idx)
+      if (categoryLSDepth3Id.style.display == 'none') categoryLSDepth3Id.style.display = 'block'
+      else categoryLSDepth3Id.style.display = 'none'
+    },
+  },
+  computed: {
+    categoryLS: function() {
+      return this.$store.getters.categoryLS
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
--- ite5-fe-mobile/src/store/index.js
+++ ite5-fe-mobile/src/store/index.js
@@ -9,12 +9,19 @@ export default new Vuex.Store({
     // 로딩상태 표시
     loading: false,
     brandIndex: [],
+    // 카테고리 항목 관리
     // WOMEN
     categoryWOMEN: {},
     categoryWOMENKeys: [],
     // MEN
     categoryMEN: {},
     categoryMENKeys: [],
+    // KIDS
+    categoryKIDS: {},
+    categoryKIDSKeys: [],
+    // LIFE STYLE
+    categoryLS: {},
+    categoryLSKeys: [],
     products: [],
   },
   mutations: {
@@ -24,6 +31,7 @@ export default new Vuex.Store({
     GET_BRAND_LIST: function(state, data) {
       state.products = data
     },
+    // 카테고리
     GET_CATEGORY_WOMEN: function(state, data) {
       for (let key in Object.values(data)) {
         state.categoryWOMEN = Object.values(data)[key]
@@ -36,10 +44,28 @@ export default new Vuex.Store({
       }
       state.categoryMENKeys = Object.keys(state.categoryMEN)
     },
+    GET_CATEGORY_KIDS: function(state, data) {
+      for (let key in Object.values(data)) {
+        state.categoryKIDS = Object.values(data)[key]
+      }
+      state.categoryKIDSKeys = Object.keys(state.categoryKIDS)
+    },
+    GET_CATEGORY_LS: function(state, data) {
+      for (let key in Object.values(data)) {
+        state.categoryLS = Object.values(data)[key]
+      }
+      state.categoryLSKeys = Object.keys(state.categoryLS)
+    },
     GET_CATEGORY_WOMEN_LIST: function(state, data) {
       state.products = data
     },
     GET_CATEGORY_MEN_LIST: function(state, data) {
+      state.products = data
+    },
+    GET_CATEGORY_KIDS_LIST: function(state, data) {
+      state.products = data
+    },
+    GET_CATEGORY_LS_LIST: function(state, data) {
       state.products = data
     },
     CHANGE_LIKE_STATUS: function(state, product) {
@@ -87,6 +113,32 @@ export default new Vuex.Store({
       })
         .then((res) => {
           context.commit('GET_CATEGORY_MEN', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    // KIDS카테고리 목록 가져오기
+    getCategoryKIDS: function(context) {
+      axios({
+        method: 'get',
+        url: 'http://localhost:8080/navbar/categoryList?depth1=KIDS'
+      })
+        .then((res) => {
+          context.commit('GET_CATEGORY_KIDS', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    // LIFE STYLE카테고리 목록 가져오기
+    getCategoryLS: function(context) {
+      axios({
+        method: 'get',
+        url: 'http://localhost:8080/navbar/categoryList?depth1=LIFESTYLE'
+      })
+        .then((res) => {
+          context.commit('GET_CATEGORY_LS', res.data)
         })
         .catch((err) => {
           console.log(err)
@@ -158,6 +210,48 @@ export default new Vuex.Store({
           this.state.loading = false
         })
     },
+    // KIDS대분류 소속의 상품 리스트 가져오기
+    getCategoryKIDSList: function(context, str) {
+      // 로딩중 중복요청 제한
+      if (this.state.loading) {
+        return
+      }
+      this.state.loading = true
+      axios({
+        method: 'get',
+        url: `http://localhost:8080/list/category${str}`
+      })
+        .then((res) => {
+          context.commit('GET_CATEGORY_KIDS_LIST', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally(() => {
+          this.state.loading = false
+        })
+    },
+    // LIFE STYLE대분류 소속의 상품 리스트 가져오기
+    getCategoryLSList: function(context, str) {
+      // 로딩중 중복요청 제한
+      if (this.state.loading) {
+        return
+      }
+      this.state.loading = true
+      axios({
+        method: 'get',
+        url: `http://localhost:8080/list/category${str}`
+      })
+        .then((res) => {
+          context.commit('GET_CATEGORY_LS_LIST', res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+        .finally(() => {
+          this.state.loading = false
+        })
+    },
     changeLikeStatus: function(context, product) {
       context.commit('CHANGE_LIKE_STATUS', product)
     }
@@ -177,6 +271,18 @@ export default new Vuex.Store({
     },
     categoryMENKeys: function(state) {
       return state.categoryMENKeys
+    },
+    categoryKIDS: function(state) {
+      return state.categoryKIDS
+    },
+    categoryKIDSKeys: function(state) {
+      return state.categoryKIDSKeys
+    },
+    categoryLS: function(state) {
+      return state.categoryLS
+    },
+    categoryLSKeys: function(state) {
+      return state.categoryLSKeys
     },
   },
   modules: {
