ac1edcecf730b7d8ca6d65d18a5ca29de6660789
blossom4 <cho8213456@gmail.com>
2021-11-29 15:37:53 +0900

    feat(payment): apply coupon discount and mileage use and user rate

--- ite5-fe-mobile/src/components/mypage/coupon.vue
+++ ite5-fe-mobile/src/components/mypage/coupon.vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <h3>Coupon</h3>
+  <div class="ms-3 mt-4">
+    <h3 class="fw-bold my-3">쿠폰 정보</h3>
+    <mycouponList v-for="(coupon, cpidx) in couponList" :key="cpidx" :coupon="coupon" :cpidx="cpidx" />
   </div>
 </template>
 
 <script>
+import mycouponList from '@/components/mypage/mycouponList'
+
 export default {
   name: 'coupon',
+  components: {
+    mycouponList,
+  },
+  computed: {
+    couponList: function() {
+      return this.$store.state.couponList
+    }
+  },
 }
 </script>
 
---
+++ ite5-fe-mobile/src/components/mypage/mycouponList.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <!-- 쿠폰번호 -->
+    <div class="d-flex  my-2">
+      <div class="col-3">
+        <h5>번호</h5>
+      </div>
+      <div class="col-9">
+        <h6>{{ coupon.cpid }}</h6>
+      </div>
+    </div>
+    <!-- 쿠폰이름 -->
+    <div class="d-flex  my-2">
+      <div class="col-3">
+        <h5>이름</h5>
+      </div>
+      <div class="col-9">
+        <h6>{{ coupon.ecoupontitle }}</h6>
+      </div>
+    </div>
+    <!-- 할인율 -->
+    <div class="d-flex  my-2">
+      <div class="col-3">
+        <h5>할인율</h5>
+      </div>
+      <div class="col-9">
+        <h6>{{ coupon.ediscount }}%</h6>
+      </div>
+    </div>
+    <!-- 쿠폰 유효기간 -->
+    <div class="d-flex  my-2">
+      <div class="col-3">
+        <h5>유효기간</h5>
+      </div>
+      <div class="col-9">
+        <h6>{{ couponIssueDate }} ~ {{ couponExpireDate }}</h6>
+      </div>
+    </div>
+    <!-- 쿠폰사용여부 -->
+    <div class="d-flex  my-2">
+      <div class="col-9">
+        <h6 v-if="coupon.cpstatus == 1" class="fw-bold" style="color: #b1d3c5;">사용가능</h6>
+        <h6 v-if="coupon.cpstatus == 0" class="fw-bold" style="color: #e4beb3;">사용완료</h6>
+      </div>
+    </div>
+    <hr>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'mycouponList',
+  props: {
+    coupon: {
+      type: Object
+    },
+    cpidx: {
+      type: Number
+    },
+  },
+  computed: {
+    couponIssueDate: function() {
+      return this.coupon.cpissuedate.substring(0, 10)
+    },
+    couponExpireDate: function() {
+      return this.coupon.cpexpiredate.substring(0, 10)
+    },
+  },
+}
+</script>
+
+<style>
+
+</style>
